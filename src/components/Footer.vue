f601acbde5c5b2c038f0a257753f6bb42795d2bb
Bozhidar Kabzamalov <bojidar_s_k@abv.bg>
2022-12-03 23:49:43 +0200

    Added some links to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,35 @@
 <template>
     <div class="footer">
         <div class="wrapper">
-            <div class="copyright">
-                <p class="copyright-text">
-                    Copyright © {{ currentYear }}. All rights reserved.
-                </p>
-                <a class="email" href="mailto: contact@rtmministries.com"
-                    >contact@rtmministries.com</a
-                >
-                <div class="logos">
-                    <div class="logo-container">
-                        <img src="@/assets/images/logo1.png" alt="Acci" />
+            <div class="container">
+                <div class="copyright">
+                    <p class="copyright-text">
+                        Copyright © {{ currentYear }}. All rights reserved.
+                    </p>
+                    <a class="email" href="mailto: contact@rtmministries.com"
+                        >contact@rtmministries.com</a
+                    >
+                    <a
+                        class="policies"
+                        href="https://www.acci.org.au/policy-resources/"
+                        target="_blank"
+                        >ACCI Policies & Resources</a
+                    >
+                    <div class="logos">
+                        <a
+                            class="logo-container"
+                            href="https://www.acci.org.au/our-global-reach/#field-workers"
+                            target="_blank"
+                            ><img src="@/assets/images/logo1.png" alt="Acci"
+                        /></a>
+                        <div class="logo-container">
+                            <img src="@/assets/images/logo2.png" alt="Rtm" />
+                        </div>
                     </div>
-                    <div class="logo-container">
-                        <img src="@/assets/images/logo2.png" alt="Rtm" />
-                    </div>
+                </div>
+                <div class="registration-numbers">
+                    <p>Raising the Mighty PHBN: CN 201 520 552</p>
+                    <p>ACCI Missions & Relief ABN: 66 077 367 223</p>
                 </div>
             </div>
         </div>
@@ -32,6 +47,9 @@ export default {
 </script>
 
 <style>
+.container {
+    padding: 10px;
+}
 .footer {
     color: #ffffff;
     display: flex;
@@ -45,7 +63,17 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 10px;
+}
+.registration-numbers {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.registration-numbers > p {
+    margin-right: 20px;
+}
+.registration-numbers > p:last-child {
+    margin-right: 0px;
 }
 .copyright-text {
     margin: 0 20px 0 0;
@@ -54,8 +82,12 @@ export default {
     color: #ffffff;
     margin-right: 20px;
 }
+.policies {
+    color: #ffffff;
+}
 .logos {
     display: flex;
+    margin-left: 20px;
 }
 .logo-container {
     width: 40px;
@@ -79,6 +111,20 @@ export default {
         margin-bottom: 10px;
     }
     .logos {
+        margin-left: 0px;
+        margin-bottom: 10px;
+    }
+    .registration-numbers {
+        flex-direction: column;
+    }
+    .registration-numbers > p {
+        margin-right: 0px;
+        margin-bottom: 10px;
+    }
+    .registration-numbers > p:last-child {
+        margin-bottom: 0px;
+    }
+    .policies {
         margin-bottom: 10px;
     }
 }
